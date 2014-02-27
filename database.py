0b1baec6be8c70d6d22a3435ead85c6fd15a25c6
c24b <4barbes@gmail.com>
2014-02-27 13:51:04 +0100

    Changing database models

--- database.py
+++ database.py
@@ -20,15 +20,9 @@ class Database():
 	def select(self, coll_name):
 		return self.db[str(coll_name)]
 		
-	def good_status(self,type, value):
-		'''insert into report status true url and type url then clean queue'''
-		self.report.insert({"url": value,"type":type, "status": True})
-		self.queue.remove({ "url": value })
-		return self
-		
-	def bad_status(self, type, url, error_type):
+	def bad_status(self, url, error_code, error_type):
 		'''insert into report url status false, url and type url then clean queue'''
-		self.report.insert({"url": url, "status": False, "type":type, "error":error_type})	
+		self.report.insert{'url':url,'error_code':error_code, 'error_type': error_type}) 
 		self.queue.remove({"url": url })
 		return self
 		
@@ -38,10 +32,9 @@ class Database():
 		size = "\n\tsize of DB: %d" % ((self.db.command('dbStats', 1024)['storageSize'])/1024)
 		work = "\n\t-url en cours de traitement: %d " % (self.queue.count()) 
 		url = "\n\t-urls traitées: %d" % (self.report.count())
-		ok = "\n\t\t%d urls correctes" % (self.report.find({"status":True}).count())
-		error = "\n\t\t %d urls fausses" % (self.report.find({"status":False}).count())
+		error = "\n\t\t %d urls erronées" % (self.report.count())
 		delim2 = "\n***STATISTIQUES du CRAWTEXT***"
-		results = "\n\t-forum: %d " % (self.results.count())
+		results = "\n\t-Nombre de liens indexés: %d " % (self.results.count())
 		result = [delim, size, work, url, ok, error, delim2, results]
 		return "".join(result)
 	
@@ -49,25 +42,9 @@ class Database():
 		if collection_name is not None:
 			print "results doc type example:", (self.results).find_one()
 		else:
-			print "No collection specified"
+			print "No collection specified. \n====>Displaying full stats..."
 			print self.get_txt_report()
-			
-	def insert_into(self, collection_name, *values):
-		#~ print "inserting", type(collection_name)
-		#~ self.db[collection_name].insert(*values, upsert=True, multi=True)
-		if collection_name == "forum":
-			return self.forum.insert(values, upsert=True, multi=True)
-		elif collection_name == "category":
-			return self.category.insert(values, upsert=True, multi=True)
-		elif collection_name == "topic":
-			return self.topic.insert(values, upsert=True, multi=True)
-		elif collection_name == "post":
-			return self.post.insert(values, upsert=True, multi=True)
-		else:
-			print "No collection specified"
-			return None
-			
-	def find_into(self, collection_name, *values):
+				def find_into(self, collection_name, *values):
 		self.db[str(collection_name)].find_one(values)
 	
 	def udpate_into(self, collection_name, value,  *query):
