3191166f5622cafee965037c3ea48c13faf2f743
C2_4b <4barbes@gmail.com>
2014-10-22 17:19:24 +0200

    remoduling call

--- crawtext.py
+++ crawtext.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+__version__ = '3.1.0'
+__author__ = 'Constance de Quatrebarbes'
+__name__ = 'crawtext'
+__all__ = ['worker', 'jobs', 'extractor']
+__doc__ = '''Crawtext.
 
-'''Crawtext.
 Description:
 A simple crawler in command line.
 
 Usage:
 	crawtext.py (<name>|<user>|<url>)
 	crawtext.py <url>  [ --format=(default|wiki|forum) ]
-	crawtext.py <name> start
-	crawtext.py <name> stop
-	crawtext.py <name> delete
-	crawtext.py <name> stats
+	crawtext.py <name> (start | stop | delete | stats | debug |list)
 	crawtext.py <name> schedule --repeat=<repeat>
 	crawtext.py <name> unschedule [--task=<task>]
-	crawtext.py <name> report [--format=<format>]
-	crawtext.py <name> export [--format= --coll_type=<coll_type>]
-	crawtext.py <name> debug
-	crawtext.py <name> list
+	crawtext.py <name> (report|export) [--format=<format>] [--coll_type=<coll_type>]
 	crawtext.py <name> [--user=<email>] [--query=<query>] [--key=<key>] [--repeat=<repeat>]
 	crawtext.py <name> -s add (<url>|<file>)
 	crawtext.py <name> -s expand
@@ -30,16 +28,18 @@ Help:
 #export --format=(csv|json)  	
 '''
 
-
-
+from datetime import datetime as dt
 from docopt import docopt
 from worker import Worker
-import os, sys
 
-ABSPATH = os.path.dirname(os.path.abspath(sys.argv[0]))
+user_input = docopt(__doc__)
 
-if __name__== "__main__":
+print __name__
+if __name__ == "crawtext":
+	print "Lauching crawtext"
 	try:		
-		w = Worker(docopt(__doc__))		
+		w = Worker(user_input)
+		start4 = dt.now()
+		
 	except KeyboardInterrupt:
 		sys.exit()
