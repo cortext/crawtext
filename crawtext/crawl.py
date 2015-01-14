d05b6e291d4349dbab13357fcd98b276b36feb1d
C2_4b <4barbes@gmail.com>
2015-01-14 13:36:43 +0100

    remove logs

--- crawtext/crawl.py
+++ crawtext/crawl.py
@@ -4,7 +4,6 @@ from article import Article, Page
 from datetime import datetime
 
 def crawl(db_name, query, directory, max_depth, debug=True):
-    print "Crawl"
     db = Database(db_name)
     db.create_colls()
     datenow = datetime.now()
@@ -20,20 +19,16 @@ def crawl(db_name, query, directory, max_depth, debug=True):
                     db.queue.remove(item)
                 else:
                     db.results.update({"url": item["url"]}, {"$push":  {"date":datenow}, "$inc": {"crawl_nb":+1}})
-                    db.queue.insert([{'url': url} for url in exists["cited_links"]])
-                
-            
+                    next_urls = []
+                    for url in exists["cited_links"]:
+                        if url not in db.distinct("url"):
+                            next_urls.append({"url": url, "source_url":exists["url"]})
+                    db.queue.insert(next_urls)
             else:
                 try:
                     p = Page(item["url"], item["source_url"],item["depth"], debug)
                 except KeyError:
                     p = Page(item["url"])
-                
-                treated.append(item["url"])
-                
-                if debug: print "position", treated.index(item["url"])
-            #and item["url"] not in db.results.distinct("url") and item["url"] not in db.logs.distinct("url") :
-                if debug is True: print "Page:"
                 if p.download(max_depth):
                     a = Article(p.url,p.html, p.source_url, p.depth, debug)
                     if a.extract() and a.filter(query, directory):
@@ -52,19 +47,15 @@ def crawl(db_name, query, directory, max_depth, debug=True):
                         else:    
                             if debug: print "max_depth exceeded %d"(self.depth)
 
-                        if debug is True: 
-                            print "\t-exported"
-                            print a.export()
                         db.insert_results(a.export())
-                        db.queue.remove(item)
+                        
+            treated.append(item["url"])
             db.queue.remove(item)          
             if debug:
                 print db.queue.count()
             if db.queue.count() == 0:
                 break
-            # if db.results.count > 100.000:
-            #     break
-
+            
         if db.queue.count() == 0:
             break
     return True
