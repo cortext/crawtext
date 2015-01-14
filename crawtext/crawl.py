af9207289f35a59cb656344069d1c0b00ffe9846
C2_4b <4barbes@gmail.com>
2015-01-14 13:27:50 +0100

    remove logs

--- crawtext/crawl.py
+++ crawtext/crawl.py
@@ -1,61 +1,71 @@
 #!/usr/bin/env python2.7
 from database import Database
 from article import Article, Page
+from datetime import datetime
 
-
-def crawl(db_name, query, directory, max_depth, debug=False):
-    debug = True
+def crawl(db_name, query, directory, max_depth, debug=True):
+    print "Crawl"
     db = Database(db_name)
     db.create_colls()
-    treated = set()
+    datenow = datetime.now()
+    treated = db.results.find()
+    print len(treated)
     while db.queue.find() > 0:
-        print "\nCrawling ..."
-        
-        for item in db.queue.find():    
-            if item["url"] not in treated and item["url"] not in db.results.distinct("url") and item["url"] not in db.logs.distinct("url") :
-                p = Page(item["url"], item["source_url"],item["depth"], debug)
-                if debug is True: print "Page:"
-                try:
-                    if p.is_valid(max_depth) and p.fetch():
-                        if debug is True: print "\t-fetched"
-                        a = Article(p.url,p.html, p.source_url, p.depth, debug)
-                        
-                        if a.extract() and a.filter(query, directory):
-                            if debug is True: print "\t-is relevant"                   
-                            print item["depth"], max_depth
-                            if item["depth"] > max_depth:
-                                a.fetch_links()        
-                                print "max_depth exceeded"
-                            else:
-                                a.get_outlinks()
-                                if debug is True: print "nexts links: %d" %len(a.outlinks)    
-                                if len(a.outlinks) > 0:
-                                    if debug is True: print "\t-next pages: %d" %len(a.outlinks)
-                                    db.queue.insert(a.outlinks)
-                            
-                            if debug is True: print "\t-exported"
-                            db.results.insert(a.export())
-                            
-                        else:
-                            if debug is True: print "\t-not relevant"                   
-                            db.insert_log(a.log())
-                        
-                except Exception as ex:
-                    p.msg = str(ex)
-                    p.code= "600"
-                    p.step = "Parsing error"
-                    db.insert_log(p.log())
-                treated.add(item["url"])
+        if debug: print "\nCrawling ..."
+        for item in db.queue.find():
+            if item["url"] in treated:
+                if debug: print "treated"
+                exists = db.results.find_one({"url":item["url"]})
+                if exists is None:
+                    db.queue.remove(item)
+                else:
+                    db.results.update({"url": item["url"]}, {"$push":  {"date":datenow}, "$inc": {"crawl_nb":+1}})
+                    db.queue.insert([{'url': url} for url in exists["cited_links"]])
+                
+            
             else:
-                print "Already treated. Removing"
-            db.queue.remove(item)
-                      
-            if debug: 
+                try:
+                    p = Page(item["url"], item["source_url"],item["depth"], debug)
+                except KeyError:
+                    p = Page(item["url"])
+                
+                treated.append(item["url"])
+                
+                if debug: print "position", treated.index(item["url"])
+            #and item["url"] not in db.results.distinct("url") and item["url"] not in db.logs.distinct("url") :
+                if debug is True: print "Page:"
+                if p.download(max_depth):
+                    a = Article(p.url,p.html, p.source_url, p.depth, debug)
+                    if a.extract() and a.filter(query, directory):
+                        a.date = datenow
+                        if debug is True: print "\t-is relevant"
+                        # print item["depth"], max_depth
+                        if item["depth"] < max_depth:
+                            if debug:print "Next" 
+
+                            a.fetch_links()
+                            if debug is True: print "nexts links: %d" %len(a.outlinks)    
+                            if len(a.outlinks) > 0:
+                                if debug is True: print "\t-next pages: %d" %len(a.outlinks)
+                                db.queue.insert(a.outlinks)
+
+                        else:    
+                            if debug: print "max_depth exceeded %d"(self.depth)
+
+                        if debug is True: 
+                            print "\t-exported"
+                            print a.export()
+                        db.insert_results(a.export())
+                        db.queue.remove(item)
+            db.queue.remove(item)          
+            if debug:
                 print db.queue.count()
             if db.queue.count() == 0:
                 break
+            # if db.results.count > 100.000:
+            #     break
+
         if db.queue.count() == 0:
             break
-        
     return True
-        
+       
