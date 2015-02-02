348879652d48b2e9cd0112ae0a23e0d857536eec
C2_4b <4barbes@gmail.com>
2015-02-02 13:44:11 +0100

    More control flow on treated item

--- crawtext/crawl.py
+++ crawtext/crawl.py
@@ -4,9 +4,11 @@ from article import Article, Page
 from datetime import datetime
 
 def uniq(seq):
-    seen = set()
-    seen_add = seen.add
-    return [ x for x in seq if not (x in seen or seen_add(x))]
+    checked = []
+    for e in seq:
+        if e not in checked:
+            checked.append(e)
+    return checked
 
 def crawl(db_name, query, directory, max_depth, debug=True):
     db = Database(db_name)
@@ -37,7 +39,7 @@ def crawl(db_name, query, directory, max_depth, debug=True):
                     except:
                         pass
                 for n in db.queue.find({"url":item["url"]}):
-                    db.queue.remove(item)
+                    db.queue.remove(n)
             if db.queue.count() == 0:
                 break
             print "Results", db.results.count()
@@ -57,6 +59,7 @@ def update_result(db,treated, item, debug):
     if exists is not None:
         if debug: print "Already treated"
         next_urls = []    
+        
         next = [n for n in exists["cited_links"] if n not in treated]
         if len(next) > 0:
             for url in exists["cited_links"]: 
@@ -70,33 +73,34 @@ def update_result(db,treated, item, debug):
     return False
 
 def create_result(db, treated, item, query, directory,max_depth, debug):
-    if debug:print "create", item["url"]
-    p = Page(item["url"], item["source_url"],item["depth"], item["date"], debug)
-    if p.download():
-        if debug: print ">Page downloaded"
-        a = Article(p.url,p.html, p.source_url, p.depth,p.date, debug)
-        if a.extract() and a.filter(query, directory):
-            if debug is True: print "\t-is relevant"
-            if a.check_depth(max_depth):
-                if debug: print "\t-page depth:", item["depth"],"<", max_depth
-                a.fetch_links()
-                if len(a.links) > 0:
-                    if debug is True: print "\t-next pages: %d" %len(a.links)
-                    db.queue.insert([{"url": url, "source_url": item['url'], "depth": int(item['depth'])+1, "domain": domain, "date": a.date} for url, domain in zip(a.links, a.domains)])
-                    print "\t-inserted %d nexts url" %len(a.links)
-                # else:
-                #     if debug: print "\t-page depth:", item['depth'],"<", max_depth
+    if debug:print "creating?", item["url"]
+    if item["url"] not in db.logs.distinct("url"):
+        p = Page(item["url"], item["source_url"],item["depth"], item["date"], debug)
+        if p.download():
+            if debug: print ">Page downloaded"
+            a = Article(p.url,p.html, p.source_url, p.depth,p.date, debug)
+            if a.extract() and a.filter(query, directory):
+                if debug is True: print "\t-is relevant"
+                if a.check_depth(max_depth):
+                    if debug: print "\t-page depth:", item["depth"],"<", max_depth
+                    a.fetch_links()
+                    if len(a.links) > 0:
+                        if debug is True: print "\t-next pages: %d" %len(a.links)
+                        db.queue.insert([{"url": url, "source_url": item['url'], "depth": int(item['depth'])+1, "domain": domain, "date": a.date} for url, domain in zip(a.links, a.domains)])
+                        print "\t-inserted %d nexts url" %len(a.links)
+                    # else:
+                    #     if debug: print "\t-page depth:", item['depth'],"<", max_depth
 
-            db.insert_result(a.export())
+                db.insert_result(a.export())
+                if item["url"] not in treated:
+                    treated.append(item["url"])
+                return (True, "")
+            else:
+                return (False, a.log())        
+        else:
             if item["url"] not in treated:
                 treated.append(item["url"])
-            return (True, "")
-        else:
-            return (False, a.log())        
-    else:
-        if item["url"] not in treated:
-            treated.append(item["url"])
-        print p.log()
-        return (False, p.log())
+            print p.log()
+            return (False, p.log())
     
     
