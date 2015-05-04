cfa05590aa6542dbeba85e26e54fe4c9544d558a
Constance de Quatrebarbes <4barbes@gmail~.com>
2015-05-04 17:26:05 +0200

    rebuilding the whole stuff

--- crawtext/saveoursoul.py
+++ crawtext/saveoursoul.py
@@ -11,14 +11,15 @@ Description:
 A simple crawler in command line for targeted websearch.
 
 Usage:
-	crawtext.py (<name>)
-	crawtext.py <name> --query=<query> --key=<key> [--nb=<nb>] [--lang=<lang>] [--user=<email>] [--depth=<depth>] [--debug]
-	crawtext.py <name> (--url=<url>| --file=<file>) [--query=<query>] [--nb=<nb>] [--lang=<lang>] [--user=<email>] [--depth=<depth>] [--debug]
-	crawtext.py (<name>) delete
-	crawtext.py (<name>) start
-	crawtext.py (<name>) restart
-	crawtext.py (<name>) export [--data=<data>] [--format=<format>]
-	crawtext.py (<name>) report [--user=<email>] [--format=<format>]
+	crawtext.py (<name>) [--debug]
+	crawtext.py (<name>) --query=<query> --key=<key> [--nb=<nb>] [--lang=<lang>] [--user=<email>] [--depth=<depth>] [--debug] [--repeat=(day|week|month)]
+	crawtext.py (<name>) (--url=<url>| --file=<file>) [--query=<query>] [--nb=<nb>] [--lang=<lang>] [--user=<email>] [--depth=<depth>] [--debug]
+	crawtext.py (<name>) delete [--debug]
+	crawtext.py (<name>) start [--debug]
+	crawtext.py (<name>) restart [--debug]
+	crawtext.py (<name>) export [--data=<data>] [--format=<format>] [--debug]
+	crawtext.py (<name>) report [--user=<email>] [--format=<format>] [--debug]
+	crawtext.py (<name>) stop [--debug]
 '''
 import os, sys, re
 import copy
@@ -41,26 +42,118 @@ MAX_RESULTS = 1000
 #max depth
 DEPTH = 100
 
-class Analyzor(object):
-	def __init__(self, name, debug):
+class Crawtext(object):
+	def __init__(self, name, user_input, debug):
 		self.name = name
 		self.debug = debug
 		self.task_db = TaskDB()
 		self.coll = self.task_db.coll
 		self.task = self.coll.find_one({"name":name})
+		self.project_path = os.path.join(RESULT_PATH, name)
 		if self.task is None:
 			self.active = False
+			sys.exit("No configuration found for project %s" %self.name)
 		else:
 			self.active = True
 
-		delattr(self, "task_db")
-		delattr(self, "coll")
+			delattr(self, "task_db")
+			delattr(self, "coll")
+			self.load_ui(user_input)
+			self.dispatch()
+
+
+	def dispatch(self):
+		if self.stop is True:
+			print "stop"
+			raise NotImplementedError
+		elif self.restart is True:
+			print "restart"
+			raise NotImplementedError
+		elif self.export is True:
+			print "export"
+			raise NotImplementedError
+		elif self.report is True:
+			print "report"
+			raise NotImplementedError
+		else:
+
+			if (self.url ^ self.file ^ self.key) is False:
+				self.show_task()
+				self.show_stats()
+				return sys.exit()
+			else:
+				self.load_config()
+				self.start()
+				return sys.exit()
+
+	def load_ui(self, ui):
+		for k, v in ui.items():
+			k = re.sub("--|<|>", "", k)
+			print k
+			if k == "debug" and self.debug is True:
+				#forcing to consider first the debug of the program
+				pass
+			if type(v) == list:
+				for item in v:
+					if type(item) == list:
+						#if it's a list we just map the last value
+						if item[-1] is not None:
+							setattr(self, k, item[-1])
+						else:
+							setattr(self, k, False)
+					else:
+						if item is None:
+							setattr(self, k, False)
+						else:
+							setattr(self, k, item)
+			else:
+				if v is None or v is False:
+					setattr(self, k, False)
+				else:
+					setattr(self, k, v)
+		return self
+
+
+	def load_config(self):
+		if self.active:
+			for k, v in self.task.items():
+				if k == "debug" and self.debug is True:
+					#forcing to consider first the debug of the program
+					pass
+
+				if type(v) == list:
+					for item in v:
+						if type(item) == list:
+							#if it's a list we just map the last value
+							if item[-1] is not None:
+								setattr(self, k, item[-1])
+							else:
+								setattr(self, k, False)
+						else:
+							if item is None:
+								setattr(self, k, False)
+							else:
+								setattr(self, k, item)
+				else:
+					if v is None or v is False:
+						setattr(self, k, False)
+					else:
+						setattr(self, k, v)
+			return self
+		else:
+			sys.exit("No configuration found for project %s") %self.name
+	def start(self):
+		logging.info("start")
+		if self.url is not False:
+			return self.set_crawler()
+		elif self.file is not False:
+			return self.set_crawler()
+		elif (self.query,self.key) != (False,False):
+			return self.set_crawler()
 
 	def show_task(self):
-		for k, v in self.task.items():
-			if k == "date":
-				pass
-			elif type(v) == list:
+		for k, v in self.__dict__.items():
+			if type(v) == list:
 				print "*", k,":"
 				for item in v:
 					if type(item) == list:
@@ -68,11 +161,24 @@ class Analyzor(object):
 							print "\t\t-", n
 					else:
 						print "\t", item
-
 			else:
 				print "*", k,":", v
-				setattr(self, k, v)
 
+		return self
+
+	def show_stats(self):
+		print "********\n"
+		print "SOURCES:"
+		print "- Nb Sources indexées: %i" %self.sources.unique
+		print "\t- pertinentes: %i" %self.sources.active_urls.nb
+		print "\t- non-pertinentes: %i" %self.sources.inactive_urls.nb
+		print "RESULTS:"
+		print "- Nb Pages indexées: %i" %self.results.unique
+		print "ERRORS:"
+		print "- Nb Pages non-indexées: %i" %self.logs.unique
+		print "PROCESSING"
+		print "- Nb Pages en cours de traitement: %i" %self.queue.unique
+		return self
 
 	def show_project(self):
 		self.project_db = Database(self.name)
@@ -85,62 +191,76 @@ class Analyzor(object):
 		self.results.nb = self.results.count()
 		self.results.urls = self.results.distinct("url")
 
-		# self.results.unique = len(self.results.urls)
-		#self.results.list = [self.results.find_one({"url":url}) for url in self.results.urls]
+		self.results.unique = len(self.results.urls)
+		self.results.list = [self.results.find_one({"url":url}) for url in self.results.urls]
 
 		#SOURCES
-		#self.sources.nb = self.sources.count()
+		self.sources.nb = self.sources.count()
 		self.sources.urls = self.sources.distinct("url")
-		#self.sources.unique = len(self.sources.urls)
+		self.sources.unique = len(self.sources.urls)
 		self.sources.active_urls = [url for url in self.sources.urls if self.sources.find_one({"url":url})["status"][-1] is True]
-		#self.sources.inactive_urls = [url for url in self.sources.urls if self.sources.find_one({"url":url})["status"][-1] is False]
-		#self.sources.errors = [self.sources.find_one({"url":url})["msg"][-1] for url in self.sources.inactive_urls]
+		self.sources.active_urls.nb = len(self.sources.active_url)
+		self.sources.inactive_urls = [url for url in self.sources.urls if self.sources.find_one({"url":url})["status"][-1] is False]
+		self.sources.inactive_urls.nb = len(self.sources.inactive_url)
+		self.sources.errors = [self.sources.find_one({"url":url})["msg"][-1] for url in self.sources.inactive_urls]
 		self.sources.list = [self.sources.find_one({"url":url}) for url in self.sources.active_urls]
 
 
 		#LOGS
-		#self.logs.nb = self.logs.count()
+		self.logs.nb = self.logs.count()
 		self.logs.urls = self.logs.distinct("url")
-		#self.logs.unique = len(self.logs.urls)
+		self.logs.unique = len(self.logs.urls)
 
-		#self.logs.errors = [self.logs.find_one({"url":url}) for url in self.logs.urls]
+		self.logs.errors = [self.logs.find_one({"url":url}) for url in self.logs.urls]
 		#QUEUE
-		#self.queue.nb = self.queue.count()
+		self.queue.nb = self.queue.count()
 		self.queue.urls = self.queue.distinct("url")
-		#self.queue.unique = len(self.queue.urls)
-		# try:
-		# 	self.queue.max_depth = max([self.queue.find_one({"url":url})["depth"] for url in self.queue.urls])
-		# except ValueError:
-		# 	pass
-		#self.queue.list = [self.queue.find_one({"url":url}) for url in self.queue.urls]
+		self.queue.unique = len(self.queue.urls)
+		try:
+			self.queue.max_depth = max([self.queue.find_one({"url":url})["depth"] for url in self.queue.urls])
+		except ValueError:
+			pass
+		self.queue.list = [self.queue.find_one({"url":url}) for url in self.queue.urls]
 		#CRAWL INFOS
 		self.crawl = self.project_db.use_coll('info')
-		#self.crawl.nb = len(self.sources.find_one({"url":self.sources.urls[-1]})["status"])
-		#self.crawl.dates = [date[0].strftime("%d-%m-%Y@%H:%M:%S") for date in self.sources.find_one({"url":self.sources.urls[-1]})["date"]]
-		#self.crawl.last_date = self.crawl.dates[-1]
-		#self.crawl.first_date = self.crawl.dates[0]
-		#self.crawl.max_depth = max([self.results.find_one({"url":url})["depth"] for url in self.results.urls])
-
-		for url in self.results.urls:
-			item = self.results.find_one({"url":url})
-			if item["depth"] <=2:
-				self.results_max_depth2.append(item["depth"])
-
+		self.crawl.nb = len(self.sources.find_one({"url":self.sources.urls[-1]})["status"])
+		self.crawl.dates = [date[0].strftime("%d-%m-%Y@%H:%M:%S") for date in self.sources.find_one({"url":self.sources.urls[-1]})["date"]]
+		self.crawl.last_date = self.crawl.dates[-1]
+		self.crawl.first_date = self.crawl.dates[0]
+		self.crawl.max_depth = max([self.results.find_one({"url":url})["depth"] for url in self.results.urls])
 		return self
 
 	def set_crawler(self):
 		if (self.query, self.key, self.url, self.file) == (False, False, False, False):
-			logging.info("Invalid parameters task should have at least one seed (file, url or API key for search)")
-			return sys.exit(1)
+			#logging.info("Invalid parameters task should have at least one seed (file, url or API key for search)")
+			return sys.exit("Invalid parameters task should have at least one seed (file, url or API key for search)")
 		elif (self.query, self.key, self.url) == (False, False, True):
-			self.crawl_type = "open_crawl"
-			return self
+			self.upsert_url(self.url, "manual")
+			return self.crawler(filter="off")
+
 		elif (self.query, self.key, self.file) == (False, False, True):
-			self.crawl_type = "open_crawl"
-			return self
-		elif (self.query, self.key) == (True, True):
-			self.crawl_type = "targeted_crawl"
-			return self
+			self.upsert_file()
+			return self.crawler(filter="off")
+
+		elif (self.query, self.key) != (False, False):
+			if self.sources.unique == 0:
+				self.get_seeds()
+			elif self.repeat is not False:
+				self.update_seeds()
+			else:
+				pass
+			return self.crawler(filter="on")
+		else:
+			if self.query is False:
+				sys.exit("No query provided. Please add one")
+			elif self.key is False:
+				sys.exit("No BING API key provided")
+			else:
+				sys.exit("Invalid parameters")
+
+	def update_crawl(self):
+		raise NotImplementedError
+
 	def bing_search(self):
 		web_results = []
 		for i in range(0,self.nb, 50):
@@ -165,41 +285,81 @@ class Analyzor(object):
 		if len(web_results) == 0:
 			return False
 		return web_results
-	def get_seeds(self):
-		if self.query is False:
-			logging.info("No query provided")
-			sys.exit(1)
-		elif self.key is False:
-			logging.info("No key provided")
-			sys.exit(1)
 
-		elif self.query is not False and self.key is not False:
+	def get_seeds(self):
+		try:
 			if self.nb is False:
 				self.nb = MAX_RESULTS
+		except AttributeError:
+			self.nb = MAX_RESULTS
 
-			if self.nb > MAX_RESULTS:
-				logging.warning("Maximum search results is %d results." %MAX_RESULTS)
-				self.nb = MAX_RESULTS
+		if self.nb > MAX_RESULTS:
+			logging.warning("Maximum search results is %d results." %MAX_RESULTS)
+			self.nb = MAX_RESULTS
+			logging.warning("Maximum search results has been set to %d results." %MAX_RESULTS)
 
-			if self.nb%50 != 0:
-				self.nb = self.nb - (self.nb%50)
+		if self.nb%50 != 0:
+			logging.warning("Maximum search results has to be a multiple of 50 ")
+			self.nb = self.nb - (self.nb%50)
+			logging.warning("Maximum search results has been set to %d results." %self.nb)
 
-			bing_sources =  self.bing_search()
-			if bing_sources is not False:
-				total_bing_sources = len(bing_sources)
-				for i,url in enumerate(bing_sources):
-					self.sources.insert({"url":url,
-													"source_url": "search",
-													"depth": 0,
-													"nb":i,
-													"total":total_bing_sources,
-													"msg":["inserted"],
-													"code": [100],
-													"status": [True]})
-				logging.info("%i urls from BING search inserted")
+		bing_sources =  self.bing_search()
+		if bing_sources is not False:
+			total_bing_sources = len(bing_sources)
+			for i,url in enumerate(bing_sources):
+					self.sources.insert({
+											"url":url,
+											"source_url": "search",
+											"depth": 0,
+											"nb":i,
+											"total":total_bing_sources,
+											"msg":["inserted"],
+											"code": [100],
+											"status": [True]
+										})
+			logging.info("%i urls from BING search inserted")
+			return self
+		else:
+			logging.warning("Bing search for source failed")
+			return self
 
+	def upsert_url(self, url, source_url):
+		if exists is None:
+			self.project_db.sources.insert({"url":url,
+											"source_url": source_url,
+											"depth": 0,
+											"msg":["inserted"],
+											"code": [100],
+											"status": [True],
+											"date": [self.date]
+											})
+		else:
+			self.project_db.sources.update({"url":exists["url"]},
+											{"$push":{
+													"msg":"updated",
+													"code": 100,
+													"status": True,
+													"date": self.date
+													}})
 
-	def crawler(self):
+			return False
+
+	def upsert_file(self):
+		try:
+			if self.directory is not False:
+				filepath = os.path.join(self.directory, self.file)
+			else:
+				filepath = os.path.join(self.project_path, self.file)
+		except AttributeError:
+			filepath = os.path.join(PROJECT_PATH, self.file)
+
+		nb = []
+		with open(filepath, 'r') as f:
+			for url in f.readlines():
+				self.upsert_url(url, "file %s") %self.file
+		return self
+
+	def crawler(self, filter="off"):
 		self.queue.list = self.sources.list
 		while self.queue.nb > 0:
 			for item in self.queue.list:
@@ -214,8 +374,21 @@ class Analyzor(object):
 					if p.download():
 						a = Article(p.url,p.html, p.source_url, p.depth,p.date, True)
 						if a.extract():
-							#if option == "filter":
-							if a.filter(self.query, self.directory):
+							if filter == "on":
+								if a.filter(self.query, self.directory):
+									if a.check_depth(a.depth):
+										a.fetch_links()
+										if len(a.links) > 0:
+											for url, domain in zip(a.links, a.domains):
+												if url not in self.queue.urls:
+													self.queue.insert({"url": url, "source_url": item['url'], "depth": int(item['depth'])+1, "domain": domain, "date": a.date})
+													if self.debug: logging.info("\t-inserted %d nexts url" %len(a.links))
+												self.results.insert(a.export())
+									else:
+										self.logs.insert(a.log())
+								else:
+									self.logs.insert(a.log())
+							else:
 								if a.check_depth(a.depth):
 									a.fetch_links()
 									if len(a.links) > 0:
@@ -225,10 +398,7 @@ class Analyzor(object):
 												if self.debug: logging.info("\t-inserted %d nexts url" %len(a.links))
 											self.results.insert(a.export())
 								else:
-									a.msg = "Depth exceed"
 									self.logs.insert(a.log())
-							else:
-								self.logs.insert(a.log())
 						else:
 							print "Error Extracting"
 							self.logs.insert(a.log())
@@ -243,16 +413,13 @@ class Analyzor(object):
 			if self.results.count() > 200000:
 				self.queue.drop()
 				break
-
+		return self.show_stats()
 
 if __name__ == "crawtext":
-	an = Analyzor(docopt(__doc__)["<name>"], True)
-	print docopt(__doc__)["<name>"]
-	# if an.active:
-	# 	an.show_project()
-	an.show_project()
-	an.crawler()
+	c = Crawtext(docopt(__doc__)["<name>"], True)
 
-	# print an.crawl.max_depth
-	# print an.queue.max_depth
-	sys.exit()
+	# if c.active:
+	# 	c.show_task()
+	#  	c.show_project()
+	# 	c.set_crawler()
+	# 	sys.exit(0)
