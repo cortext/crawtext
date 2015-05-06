05eaec9ce6dc830b623b40fd6c7021d3083ce31d
Constance de Quatrebarbes <4barbes@gmail~.com>
2015-05-06 23:57:47 +0200

    checking syntax

--- crawtext/saveoursoul.py
+++ crawtext/saveoursoul.py
@@ -499,7 +499,7 @@ class Crawtext(object):
 
 		logging.info("Begin crawl with %i active urls"%self.sources.active.nb)
 		self.push_to_queue()
-		
+
 		logging.info("Processing %i urls"%self.queue.nb)
 
 
@@ -531,7 +531,7 @@ class Crawtext(object):
 										a.fetch_links()
 										if len(a.links) > 0:
 											for url, domain in zip(a.links, a.domains):
-												if url not in self.queue.find({"url": url}) and not in self.results.find({"url": url}):
+												if url not in self.queue.find({"url": url}).extend(self.results.find({"url": url})):
 													self.queue.insert({"url": url, "source_url": item['url'], "depth": int(item['depth'])+1, "domain": domain, "date": a.date})
 													if self.debug: logging.info("\t-inserted %d nexts url" %len(a.links))
 												if a.url not in self.results.find({"url": url}):
