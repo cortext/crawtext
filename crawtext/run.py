08e2add71941205a38f3cab433f3fc1798bc95f4
Constance de Quatrebarbes <4barbes@gmail.com>
2015-11-10 12:27:56 +0100

    Sched simple run

--- crawtext/run.py
+++ crawtext/run.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Usage: run.py (<db>| --db=<db>)
+"""Usage: run.py (<db> <coll>| --db=<db> --coll=<col>)
 
 Options:
     db     run the scheduled crawtext project from a given database
 """
-
-
-
+def get_tasks(db_name, coll_name):
+    db = Database(db_name)
+    coll = db.use_coll(coll_name)
+    return [n for n in coll.find()]
+        
 
 def run_now(last_run, interval ="day"):
     '''interval default 1 day or 1 week or 1 month'''
@@ -29,6 +31,7 @@ def run_now(last_run, interval ="day"):
     except TypeError:
         return False
 
+
 if __name__ == '__main__':
     import docopt
     
@@ -37,21 +40,19 @@ if __name__ == '__main__':
     from crawler import Crawtext
     import datetime
     db_name = docopt.docopt(__doc__)['<db>']
+    coll_name = docopt.docopt(__doc__)['<coll>']
     app = Celery('tasks', broker='mongodb://localhost:27017/%s' %db_name)
     
         
     @app.task(name='tasks.crawl')
     def add(data):
         
-            #print data["name"], run_now(data["last"], data["next"])
-            #
-                #print data["name"], data["next"], data["last"]
-            return Crawtext(data)
+        return Crawtext(data)
     
     # Parse arguments, use file docstring as a parameter definition
     
     for n in get_tasks(db_name):
-        #print n["name"]
+        print "Adding", n["name"]
         add(n)
         
     
