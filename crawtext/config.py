2fce3d0456c91d55103decfb367227065218dbe5
Constance de Quatrebarbes <4barbes@gmail.com>
2016-01-26 01:22:41 +0100

    Adding Setup CONSTANT method

--- crawtext/config.py
+++ crawtext/config.py
@@ -1,34 +1,158 @@
 #usr/bin python env
-import os
+# coding: utf-8
+import os, sys
+import simplejson as json
+import yaml
 
-def config(afile="config.json"):
-    '''load config given a YAML or a JSON file'''
-    if afile.endswith("json"):
-        import json
-        curr_dir = os.path.join(os.getcwd(), "crawtext")
-        afile = os.path.join(curr_dir, afile)
-        with open(afile) as json_f:
+import crypt
+from pymongo import MongoClient
+from collections import namedtuple
+
+class Setup(object):
+    def __init__(self, global_settings=None):
+        '''setting global environnement'''
+        self.get_settings(global_settings)
+        self.USER, self.password = self.settings["user"], crypt.crypt(self.settings["password"],"22")
+        #~ os.system("useradd -p "+self.password+" "+self.user)
+        self.db = self.settings["db"]
+        if not self.db_exists():
+            self.setup_db()
+        
+        self.DB = self.client[self.db["db_name"]]
+        self.COLL = self.DB[self.db["collection"]]
+        self.DIR = self.create_env()
+        
+        
+    def get_settings(self, cfg):
+        #loading default setup
+        if cfg is None:
+            afile = os.path.realpath("./config/settings.json")
+            self.settings = json_config(afile)
+            return self.settings
+        #loading from a simple dict
+        elif isinstance(cfg, dict):
+            self.settings = cfg
+            return self.settings
+        #loading from file
+        else:
+            afile = os.path.realpath(cfg)
+            
             try:
-                config = json.load(json_f)
-            except ValueError as e:
-                sys.exit("Error parsing config.json file: %s" %e)
+                if afile.endswith(".json"):
+                    self.settings = json_config(afile)
+                elif afile.endswith(".yml"):
+                    self.settings = yml_config(afile)
+                else:
+                    sys.exit("SETUP error: config input MUST be a dict, a JSON or a YAML file")
+            except IOError:
+                self.settings = json_config(afile)
+                sys.exit("SETUP error: no config file found")
+            return self.settings
+    
+    def setup_db(self):
+        uri = 'mongodb://%s,%s:%s'%(self.db["host"], self.db["host"], self.db["port"])
+        self.client =  MongoClient(uri)
+        DB = self.client[self.db["db_name"]]
+        COLL = DB[self.db["collection"]]
+        return self.client
+    
+    def db_exists(self):
+        uri = 'mongodb://%s,%s:%s'%(self.db["host"], self.db["host"], self.db["port"])
+        self.client =  MongoClient(uri)
+        return bool(self.db["db_name"] in self.client.database_names())
+   
+    def create_env(self):
+        '''create the default store directory'''
+        self.DIR = os.path.join(os.getcwd(), self.USER)
+        if not os.path.exists(self.DIR):
+            os.makedirs(self.DIR)
+        return self.DIR
+        
+class Config(object):
+    def __init__(self, project = None, global_settings = None):
+        if global_settings is None:
+            s = Setup()
+            SETTINGS = {"user":s.USER,"db":s.DB,"coll": s.COLL,"dir": s.DIR)
+        else:
+            s = Setup(global_settings)
+            SETTINGS = {"user":s.USER,"db":s.DB,"coll": s.COLL,"dir": s.DIR)
+            
+    def get_settings(self, cfg):
+        #loading default setup
+        if cfg is None:
+            print "Loading demo"
+            afile = os.path.realpath("./config/default.json")
+            self.settings = json_config(afile)
+            return self.settings
+        #loading from a simple dict
+        elif isinstance(cfg, dict):
+            self.settings = cfg
+            return self.settings
+        #loading from file
+        else:
+            afile = os.path.realpath(cfg)
+            
+            try:
+                if afile.endswith(".json"):
+                    self.settings = json_config(afile)
+                elif afile.endswith(".yml"):
+                    self.settings = yml_config(afile)
+                else:
+                    sys.exit("SETUP error: config input MUST be a dict, a JSON or a YAML file")
+            except IOError:
+                self.settings = json_config(afile)
+                sys.exit("SETUP error: no config file found")
+            return self.settings
+    def setup_project(self, cfg):
+        '''reading setup info db, user'''
+        if cfg is None:
+            afile = os.path.realpath("./config/default_settings.json")
+            self.settings = json_config(afile)
+            print self.settings
+        #loading from a simple dict
+        
+        elif isinstance(cfg, dict):
+            self.settings = cfg
+            return self.settings
+        #loading from file
+        
+        else:
+            afile = os.path.join(os.getcwd(), cfg)
+            print afile
+            try:
+                if afile.endswith(".json"):
+                    self.settings = json_config(afile)
+                elif afile.endswith(".yml"):
+                    self.settings = yml_config(afile)
+                else:
+                    sys.exit("CONFIG error: config input MUST be a dict, a JSON or a YAML file")
+            except IOError:
+                self.settings = json_config(afile)
+                sys.exit("CONFIG error: no config file found")
+            return self.settings
+            
+            
+    def check_format(self):
+        '''check if default settings are correctly setup'''
+        setup_params = [bool(self.params.haskey(n)) for n in ["user", "db"]]
+        print setup_params
+        print self.params.keys()
+        #~ if any(bool(self.params.haskey("user")), bool:
+            #~ print "ok"
+        
+def yml_config(afile):
+    with open(afile, 'r') as ymlfile:
+        
+        config = yaml.load(ymlfile.read())
         return config
-        
-        
-    elif afile.endswith("yml"):
-        import yaml
-        
-        curr_dir = os.path.join(os.getcwd(), "crawtext")
-        afile = os.path.join(curr_dir, afile)
+
+def json_config(afile):
+    with open(afile, 'r') as jsonfile:
         try:
-            with open(afile, 'r') as ymlfile:
-                try:
-                    config = yaml.load(ymlfile.read())
-                except ValueError as e:
-                    sys.exit("Error parsing config.yml file: %s" %e)
-            return config
-        except IOError, e:
-            sys.exit("File not found: config.yml")
-    else:
-        sys.exit("No config file found")
+            config = json.load(jsonfile)
+            return config    
+        except ValueError as e:
+            sys.exit("Error parsing %s file: %s" %(afile, e))
+        
+
 
